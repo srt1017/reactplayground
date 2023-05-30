591435f5e2dd4f88084982c5a3331d2242a8b41b
srt1017 <omarfarouq107@gmail.com>
2023-05-30 12:45:01 -0500

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
